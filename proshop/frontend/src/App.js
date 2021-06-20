604765a233846be0dd0f3ebe9b2398461c5b6e5c
skm0078 <skmukherjee1994@gmail.com>
2021-06-20 22:29:57 +0530

    A basic front end setup with dummy data is prepared

--- proshop/frontend/src/App.js
+++ proshop/frontend/src/App.js
@@ -1,11 +1,23 @@
-import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import ProductScreen from "./screens/ProductScreen";
 
-const App =()=> {
+const App = () => {
   return (
-    <div>
-      <h1>We are on</h1>
-    </div>
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
-}
+};
 
 export default App;
