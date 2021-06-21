872b92bce82d9b440c91bb8a0b5f50b3866d04b3
skm0078 <skmukherjee1994@gmail.com>
2021-06-21 18:44:05 +0530

    Database Connection established after error resolution

--- proshop/backend/server.js
+++ proshop/backend/server.js
@@ -1,17 +1,31 @@
-const express = require("express");
-const products = require("./data/products");
+// import path from 'path'
+import dotenv from "dotenv"
+import express from "express"
+import colors from "colors"
+import connectDB from "./config/db.js"
+import products from "./data/products.js"
 
-const app = express();
+dotenv.config({ path: "../.env" })
+
+const app = express()
+connectDB()
 
 app.get("/", (req, res) => {
-  res.send("API is running");
-});
+  res.send("API is running")
+})
 app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+  res.json(products)
+})
 app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+  const product = products.find((p) => p._id === req.params.id)
+  res.json(product)
+})
 
-app.listen(5000, console.log("Server running on backend server"));
+const PORT = process.env.PORT || 4000
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+)
