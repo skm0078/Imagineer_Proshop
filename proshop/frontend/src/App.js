4cf5cf863a16388d7696172f8a581893b202edc5
skm0078 <skmukherjee1994@gmail.com>
2021-06-23 19:21:32 +0530

    Unhandled Rejection (TypeError): state is not iterable || Add to Cart progress

--- proshop/frontend/src/App.js
+++ proshop/frontend/src/App.js
@@ -1,9 +1,10 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import HomeScreen from "./screens/HomeScreen";
-import { Container } from "react-bootstrap";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import ProductScreen from "./screens/ProductScreen";
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import HomeScreen from "./screens/HomeScreen"
+import { Container } from "react-bootstrap"
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import ProductScreen from "./screens/ProductScreen"
+import CartScreen from "./screens/CartScreen"
 
 const App = () => {
   return (
@@ -13,11 +14,12 @@ const App = () => {
         <Container>
           <Route path='/' component={HomeScreen} exact />
           <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/cart/:id?' component={CartScreen} />
         </Container>
       </main>
       <Footer />
     </Router>
-  );
-};
+  )
+}
 
-export default App;
+export default App
