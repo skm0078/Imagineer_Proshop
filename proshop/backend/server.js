8fd2201aded4506ee5ce3113fd3a6e395a79dee8
skm0078 <skmukherjee1994@gmail.com>
2021-06-24 18:58:22 +0530

    user Login & Reducer action initiated

--- proshop/backend/server.js
+++ proshop/backend/server.js
@@ -5,17 +5,21 @@ import colors from "colors"
 import connectDB from "./config/db.js"
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
 import productRoutes from "./routes/productRoutes.js"
+import userRoutes from "./routes/userRoutes.js"
 
 dotenv.config({ path: "../.env" })
+connectDB()
 
 const app = express()
-connectDB()
+
+app.use(express.json())
 
 app.get("/", (req, res) => {
   res.send("API is running")
 })
 
 app.use("/api/products", productRoutes)
+app.use("/api/users", userRoutes)
 
 app.use(notFound)
 
