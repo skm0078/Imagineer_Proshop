4ae3dd3c44d1f969edd2a03269d458a586e00a96
skm0078 <skmukherjee1994@gmail.com>
2021-06-25 16:34:31 +0530

    Register & Login flow complete

--- proshop/frontend/src/App.js
+++ proshop/frontend/src/App.js
@@ -5,6 +5,8 @@ import { Container } from "react-bootstrap"
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import ProductScreen from "./screens/ProductScreen"
 import CartScreen from "./screens/CartScreen"
+import LoginScreen from "./screens/LoginScreen"
+import RegisterScreen from "./screens/RegisterScreen"
 
 const App = () => {
   return (
@@ -12,9 +14,11 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <Route path='/' component={HomeScreen} exact />
+          <Route path='/register' component={RegisterScreen} />
+          <Route path='/login' component={LoginScreen} />
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
+          <Route path='/' component={HomeScreen} exact />
         </Container>
       </main>
       <Footer />
