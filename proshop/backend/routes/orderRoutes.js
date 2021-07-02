3e7bb70761e1b2302c0740b1d9f2fccbf00e0b82
skm0078 <skmukherjee1994@gmail.com>
2021-07-02 16:35:30 +0530

    Before Payment Integration Done

--- proshop/backend/routes/orderRoutes.js
+++ proshop/backend/routes/orderRoutes.js
@@ -2,9 +2,14 @@ import express from "express"
 const router = express.Router()
 import { protect } from "../middleware/authMiddleware.js"
 import {
-addOrderItems
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
 } from "../controllers/orderController.js"
 
-router.route("/").post(protect,addOrderItems)
+router.route("/").post(protect, addOrderItems)
+//make sure it is at the bottom , that are with params
+router.route("/:id").get(protect, getOrderById)
+router.route("/:id/pay").put(protect, updateOrderToPaid)
 
 export default router
